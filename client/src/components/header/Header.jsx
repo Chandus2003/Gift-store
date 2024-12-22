60cb3556342ab37603cab368a92483292719633d
Chandra Bhushan Sah <123019803+Chandus2003@users.noreply.github.com>
2024-12-22 17:52:09 +0530

    setting up header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,27 @@
-
+import { AppBar, Toolbar,styled} from '@mui/material'
 function Header() {
-
+    
+    const HeaderColor = styled(AppBar)`
+    font-family: 'Montserrat', sans-serif;
+    background: rgb(255, 215, 55);
+    margin-right: auto;
+    margin-left: auto;
+    padding-left: 15px;
+    padding-right: 15px;
+    height: 20px
+  `;
+    
     return (
         <div>
-            <h1>This is header</h1>
-            <h2>nodemoin</h2>
-            <h3>ok</h3>
+            
+            <HeaderColor>
+            <div className='Offers'>Use code FWFRIDAY to avail ₹50 off. Valid till midnight.
+            </div>
+                {/* <Toolbar>
+                    
+
+                </Toolbar> */}
+            </HeaderColor>
         </div>
 
 
