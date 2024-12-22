9ddd5234cd18f9191c5d96b8602c3a0c86379c4b
Chandra Bhushan Sah <123019803+Chandus2003@users.noreply.github.com>
2024-12-22 16:10:01 +0530

    installed Nodemon

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+
+    return (
+        <div>
+            <h1>This is header</h1>
+            <h2>nodemoin</h2>
+            <h3>ok</h3>
+        </div>
+
+
+    )
+}
+export default Header;
